01a3a7afa4b762aff3af8ab3bccffbb898bdc1b7
Ábner Silva de Oliveira <abner.oliveira@serpro.gov.br>
2015-02-10 13:32:26 -0300

    removing file

---
+++ spec/omniauth-expressov3/expressov3_auth_client_2_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+describe "OmniAuth::ExpressoV3::AuthClient" do
+
+  describe 'authenticate' do
+    it 'should authenticate' do
+      auth = OmniAuth::ExpressoV3::AuthClient.new
+      auth_data = auth.authenticate(ENV['EXPRESSO_USERNAME'], ENV['EXPRESSO_PASSWORD'])
+      expect(auth_data).not_to be(nil)
+      expect(auth_data['keys']).not_to be(nil)
+      data = auth.get_user_data
+
+      expect(data['currentAccount']).not_to be(nil)
+      expect(data['userContact']).not_to be(nil)
+
+    end
+  end
+end
